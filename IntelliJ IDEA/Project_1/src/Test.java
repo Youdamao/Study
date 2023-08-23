87042f85854c9277d450ea5a51f206e202e7f85c
MuYi <1569504035@qq.com>
2023-08-23 16:02:56 +0800

    Study

--- IntelliJ IDEA/Project_1/src/Test.java
+++ IntelliJ IDEA/Project_1/src/Test.java
@@ -1,16 +1,19 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args){
-        int [] numbers = {10, 20, 30, 40, 50};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type a number:");
+        int number = sc.nextInt();
 
-        for(int x : numbers ){
-            System.out.print( x );
-            System.out.print(",");
-        }
-        System.out.print("\n");
-        String [] names ={"James", "Larry", "Tom", "Lacy"};
-        for( String name : names ) {
-            System.out.print( name );
-            System.out.print(",");
+        for (int i = 1; i < number; i++) {
+            if (i * i == number) {
+                System.out.println(i + "is" + number + "/'s 平方根");
+                break;
+            } else if (i * i >number) {
+                System.out.println((i - 1) + "is" + number + "/'s 平方根");
+                break;
+            }
         }
     }
 }
